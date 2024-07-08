9b5f96bfad19d26bb8d27625d84b9d37972b9bd7
Soumaya Fall <soumayafall9@gmail.com>
2024-07-08 09:08:07 +0000

    Création des packages

---
+++ app/main.py
@@ -0,0 +1,36 @@
+from .extensions import api, jwt
+from .config import Config
+from flask import Flask, jsonify,request
+
+
+def create_app():
+
+    #Creating the Flask App: 
+    app=Flask(__name__)
+    app.config.from_object(Config)
+    
+    #Initializing Extensions: 
+    api.init_app(app)
+    jwt.init_app(app)
+
+    """
+
+    #Database Initialization
+
+    #Blueprint Registration: 
+
+    #JWT Error Handlers
+
+    """
+return app
+
+
+
+
+
+
+
+
+
+
+
